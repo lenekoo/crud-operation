f316a2019e6ab64b9b0d8c100bf4e2b05450d878
444elai <164175622+lenekoo@users.noreply.github.com>
2025-09-02 20:25:02 +0800

    updated

--- new.php
+++ new.php
@@ -63,14 +63,39 @@ $result = $stmt->fetchAll(PDO::FETCH_OBJ);
 
 <!-- Modal -->
 <div class="modal fade" id="insertModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
+  <div class="modal-dialog modal-dialog-centered">
     <div class="modal-content">
       <div class="modal-header">
         <h1 class="modal-title fs-5" id="exampleModalLabel">Add?</h1>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
-        ...
+        
+    <div class="row g-3">
+  <div class="col-12">
+    <input type="text" class="form-control" placeholder="First name" aria-label="First name">
+  </div>
+  <div class="col-12">
+    <input type="text" class="form-control" placeholder="Last name" aria-label="Last name">
+  </div>
+
+        <div class="row g-3">
+  <div class="col-12">
+    <input type="email" class="form-control" placeholder="Email" aria-label="Email">
+  </div>
+  <div class="col-12">
+    <input type="text" class="form-control" placeholder="Gender" aria-label="Gender>
+
+  </div> 
+  <div class="row g-3">
+  <div class="col-12">
+    <input type="text" class="form-control" placeholder="Address" aria-label="Address">
+  </div>
+  <div class="col-12">
+    <input type="date" class="form-control" placeholder="Date" aria-label="Date">
+
+  </div> 
+</div>
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
